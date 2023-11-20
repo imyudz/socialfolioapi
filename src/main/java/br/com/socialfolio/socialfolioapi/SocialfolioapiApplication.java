7bf9d92e64e57b8b626ae8ec3c8bc69a62ae70e0
Victor Henri S. Vargas <103096096+imyudz@users.noreply.github.com>
2023-11-19 22:12:18 -0300

    initial commit

---
+++ src/main/java/br/com/socialfolio/socialfolioapi/SocialfolioapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.socialfolio.socialfolioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialfolioapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialfolioapiApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/br/com/socialfolio/socialfolioapi/SocialfolioapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.socialfolio.socialfolioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialfolioapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
