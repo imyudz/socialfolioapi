afbf321ed53f2f26df27580eaa38272ad3b8ea90
Victor Henri S. Vargas <103096096+imyudz@users.noreply.github.com>
2023-12-17 20:54:39 -0300

    Readicionando configurações para acesso com CORS

--- src/main/java/br/com/socialfolio/socialfolioapi/config/CorsConfig.java
+++ src/main/java/br/com/socialfolio/socialfolioapi/config/CorsConfig.java
@@ -1,24 +1,24 @@
-// package br.com.socialfolio.socialfolioapi.config;
+package br.com.socialfolio.socialfolioapi.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
-// public class CorsConfig {
+@Configuration
+public class CorsConfig {
 
-//     @Bean
-//     public WebMvcConfigurer corsConfigurer() {
-//         return new WebMvcConfigurer() {
-//             @Override
-//             public void addCorsMappings(CorsRegistry registry) {
-//                 registry.addMapping("/**")
-//                         .allowedOrigins("*") // Permitir solicitações de qualquer origem
-//                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
-//                         .allowedHeaders("*") // Headers permitidos
-//                         .allowCredentials(true);
-//             }
-//         };
-//     }
-// }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200") // Permitir solicitações de qualquer origem
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Headers permitidos
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
